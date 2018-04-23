e96642691a8f204a405480dcd588bb8035f5bbaf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgRichEditSpellChecker/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.XtraRichEdit.Commands;
+using DevExpress.XtraSpellChecker;
+
+namespace AgRichEditSpellChecker
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            richEdit1.ApplyTemplate();
+
+            Stream dict = Assembly.GetExecutingAssembly().GetManifestResourceStream("AgRichEditSpellChecker.Dic.el_GR.dic");
+            Stream grammar = Assembly.GetExecutingAssembly().GetManifestResourceStream("AgRichEditSpellChecker.Dic.el_GR.aff");
+            SpellCheckerOpenOfficeDictionary dictionary = new SpellCheckerOpenOfficeDictionary();
+            CultureInfo ellinika = new CultureInfo("el-GR");        
+            dictionary.LoadFromStream(dict, grammar, null);
+            dictionary.Culture = ellinika;
+
+            SpellCheckerHelper.SpellChecker.Dictionaries.Add(dictionary);
+            SpellCheckerHelper.SpellChecker.Culture = ellinika;
+        }
+
+        void Open_Click(object sender, RoutedEventArgs e)
+        {
+            System.Net.WebClient webClient = new WebClient();
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+            webClient.DownloadStringAsync(new Uri("../Texts/sample.rtf", UriKind.Relative));
+            
+
+        }
+
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            richEdit1.RichControl.RtfText = e.Result;
+        }
+    }
+}
---
+++ VB/AgRichEditSpellChecker/MainPage.xaml.vb
@@ -0,0 +1,43 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Globalization
+Imports System.IO
+Imports System.Net
+Imports System.Reflection
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.XtraRichEdit.Commands
+Imports DevExpress.XtraSpellChecker
+
+Namespace AgRichEditSpellChecker
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+
+			richEdit1.ApplyTemplate()
+
+			Dim dict As Stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("el_GR.dic")
+			Dim grammar As Stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("el_GR.aff")
+			Dim dictionary As New SpellCheckerOpenOfficeDictionary()
+			Dim ellinika As New CultureInfo("el-GR")
+			dictionary.LoadFromStream(dict, grammar, Nothing)
+			dictionary.Culture = ellinika
+
+			SpellCheckerHelper.SpellChecker.Dictionaries.Add(dictionary)
+			SpellCheckerHelper.SpellChecker.Culture = ellinika
+		End Sub
+
+		Private Sub Open_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim webClient As System.Net.WebClient = New WebClient()
+			AddHandler webClient.DownloadStringCompleted, AddressOf webClient_DownloadStringCompleted
+			webClient.DownloadStringAsync(New Uri("../Texts/sample.rtf", UriKind.Relative))
+
+
+		End Sub
+
+		Private Sub webClient_DownloadStringCompleted(ByVal sender As Object, ByVal e As DownloadStringCompletedEventArgs)
+			richEdit1.RichControl.RtfText = e.Result
+		End Sub
+	End Class
+End Namespace
