fdf40a6b58a237cee298c4de445b7fec8cbd2439
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RichEditSpellCheckerSL/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.XtraRichEdit.Commands;
+using DevExpress.XtraSpellChecker;
+using RichEditSpellCheckerSL.SpellChecker;
+
+namespace RichEditSpellCheckerSL
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            richEdit1.Loaded += new RoutedEventHandler(richEdit1_Loaded);
+        }
+        #region spellcheckerinit
+        void richEdit1_Loaded(object sender, RoutedEventArgs e)
+        {
+            DevExpress.Xpf.SpellChecker.SpellChecker spellChecker = SpellCheckerHelper.SpellChecker;
+            spellChecker.SpellCheckMode = SpellCheckMode.AsYouType;
+
+            SpellCheckerOpenOfficeDictionary dictionary = LoadGreekDictionary();
+
+            SpellCheckerHelper.SpellChecker.Dictionaries.Add(dictionary);
+            SpellCheckerHelper.SpellChecker.Culture = dictionary.Culture;
+            
+            this.richEdit1.SpellChecker = spellChecker;
+        }
+
+        static SpellCheckerOpenOfficeDictionary LoadGreekDictionary()
+        {
+            Stream dict = Assembly.GetExecutingAssembly().GetManifestResourceStream("RichEditSpellCheckerSL.Dic.el_GR.dic");
+            Stream grammar = Assembly.GetExecutingAssembly().GetManifestResourceStream("RichEditSpellCheckerSL.Dic.el_GR.aff");
+            SpellCheckerOpenOfficeDictionary dictionary = new SpellCheckerOpenOfficeDictionary();
+            dictionary.LoadFromStream(dict, grammar, null);
+            dictionary.Culture = new CultureInfo("el-GR");
+            return dictionary;
+        }
+        #endregion #spellcheckerinit
+        void Open_Click(object sender, RoutedEventArgs e)
+        {
+            System.Net.WebClient webClient = new WebClient();
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+            webClient.DownloadStringAsync(new Uri("../Texts/sample.rtf", UriKind.RelativeOrAbsolute));
+        }
+        
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            richEdit1.RtfText = e.Result;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to use external dictionaries to check spelling in different languages
+
+
+<p>This example demonstrates how to check spelling of a <strong>Greek</strong> text using a Greek dictionary from the <strong>OpenOffice.org</strong> project.<br />
+A dictionary is downloaded from the OpenOffice.org wiki page and converted to <strong>utf-8</strong> as described in the <a href="https://www.devexpress.com/Support/Center/p/K18404">K18404</a> article.<br />
+When the page is loaded, the Greek dictionary is also loaded and initialized. Click the <strong> Open </strong> button to load a document containing text in Greek.<br />
+Check spelling in Greek using the <strong>Check Spelling</strong> and <strong>Check Spelling As You Type</strong> command buttons. You can switch between languages via the <strong>Spell Checker Options</strong> dialog.</p>
+
+<br/>
+
+
