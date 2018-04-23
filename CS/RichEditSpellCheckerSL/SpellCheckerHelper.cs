1549cc3be2d2b2eb66ff88c5292614968fd6000f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RichEditSpellCheckerSL/SpellCheckerHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevExpress.XtraSpellChecker;
+using System.IO;
+using System.Globalization;
+using System.Reflection;
+using DevExpress.XtraSpellChecker.Native;
+using DevExpress.Xpf.RichEdit;
+using DevExpress.XtraRichEdit.SpellChecker;
+
+namespace RichEditSpellCheckerSL.SpellChecker
+{
+    #region #spellcheckerhelper
+    public static class SpellCheckerHelper {
+        static DevExpress.Xpf.SpellChecker.SpellChecker spellChecker;
+        public static DevExpress.Xpf.SpellChecker.SpellChecker SpellChecker {
+            get {
+                if (spellChecker == null)
+                    spellChecker = CreateSpellChecker();
+                return spellChecker;
+            }
+        }
+        static DevExpress.Xpf.SpellChecker.SpellChecker CreateSpellChecker() {
+            SpellCheckTextControllersManager.Default.RegisterClass(typeof(RichEditControl), typeof(RichEditSpellCheckController));
+            DevExpress.Xpf.SpellChecker.SpellChecker spellChecker = new DevExpress.Xpf.SpellChecker.SpellChecker();
+            spellChecker.Culture = new CultureInfo("en-US");
+            try {
+                RegisterDictionary(spellChecker, GetDefaultDictionary());
+                RegisterDictionary(spellChecker, GetCustomDictionary(spellChecker));
+            }
+            catch {
+            }
+            return spellChecker;
+        }
+        static SpellCheckerDictionaryBase GetDefaultDictionary() {
+            SpellCheckerISpellDictionary dic = new SpellCheckerISpellDictionary();
+            dic.LoadFromStream(GetDataStream("american.xlg"),
+                               GetDataStream("english.aff"),
+                               GetDataStream("EnglishAlphabet.txt"));
+            return dic;
+        }
+        static Stream GetDataStream(string fileName) {
+            if (String.IsNullOrEmpty(fileName))
+                return null;
+            string path = "RichEditSpellCheckerSL.DefaultDictionary." + fileName;
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+        }
+        static void RegisterDictionary(DevExpress.Xpf.SpellChecker.SpellChecker spellChecker, SpellCheckerDictionaryBase dic) {
+            dic.Culture = spellChecker.Culture;
+            spellChecker.Dictionaries.Add(dic);
+        }
+        static SpellCheckerDictionaryBase GetCustomDictionary(DevExpress.Xpf.SpellChecker.SpellChecker spellChecker) {
+            return new SpellCheckerCustomDictionary("CustomEnglish.dic", spellChecker.Culture);
+        }
+    }
+    #endregion #spellcheckerhelper
+}
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# How to use external dictionaries to check spelling in different languages
+
+
+<p>This example demonstrates how to check spelling of a <strong>Greek</strong> text using a Greek dictionary from the <strong>OpenOffice.org</strong> project.<br />
+A dictionary is downloaded from the OpenOffice.org wiki page and converted to <strong>utf-8</strong> as described in the <a href="https://www.devexpress.com/Support/Center/p/K18404">K18404</a> article.<br />
+When the page is loaded, the Greek dictionary is also loaded and initialized. Click the <strong> Open </strong> button to load a document containing text in Greek.<br />
+Check spelling in Greek using the <strong>Check Spelling</strong> and <strong>Check Spelling As You Type</strong> command buttons. You can switch between languages via the <strong>Spell Checker Options</strong> dialog.</p>
+
+
+<h3>Description</h3>
+
+<p>Starting with v2010 vol.2 you are advised not to use the DevExpress.Xpf.RichEdit.Extensions assembly in your applications. We suggest that you use the RichEditControl control instead of the RichEdit control and implement a SpellCheckerHelper class as illustrated in this example.</p>
+
+<br/>
+
+
